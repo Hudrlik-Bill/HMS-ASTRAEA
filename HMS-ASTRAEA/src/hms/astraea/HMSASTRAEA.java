8139b0f0609f1732123b6abb53c0a816bf72e076
Hudrlik-Bill <billhudrlik@gmail.com>
2017-01-27 22:06:30 -0800

    Initial setup for WaterCraft, Backpack, Shelter, Location, Map, and Game with test.

--- HMS-ASTRAEA/src/hms/astraea/HMSASTRAEA.java
+++ HMS-ASTRAEA/src/hms/astraea/HMSASTRAEA.java
@@ -6,28 +6,90 @@
 package hms.astraea;
 
 import HMSASTRAEA.model.InventoryClass;
+import HMSASTRAEA.model.WaterCraft;
+import HMSASTRAEA.model.Backpack;
+import HMSASTRAEA.model.Shelter;
+import HMSASTRAEA.model.Location;
+import HMSASTRAEA.model.Map;
+import HMSASTRAEA.model.Game;
 
 /**
  *
  * @author Teancum
  */
-public class HMSASTRAEA 
-{
+public class HMSASTRAEA {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
-    {
-        
+    public static void main(String[] args) {
+
         InventoryClass wood = new InventoryClass();
-        
+
         wood.setItemType("ship plank");
         wood.setItemNumber(1);
         wood.setRequiredAmount(1);
-        
+
         String itemDescription = wood.toString();
         System.out.println(wood);
+
+        WaterCraft raft = new WaterCraft();
+
+        raft.setCraftLength(10);
+        raft.setCraftWidth(5);
+        raft.setCraftType("flat");
+        raft.setItemNumber(1);
+        raft.setItemType("bucket");
+        raft.setItemWeight(10);
+        raft.setLoadedWeight(20);
+        raft.setMaxWeight(100);
+
+        String craft = raft.toString();
+        System.out.println(craft);
+
+        Backpack brown = new Backpack();
+        
+        brown.setItemType("food");
+        brown.setItemWeight(10);
+        brown.setItemNumber(1);
+        brown.setLoadedWeight(15);
+        brown.setMaxWeight(50);
+        
+        String bag = brown.toString();
+        System.out.println(bag);
+        
+        Shelter hut = new Shelter();
+        
+        hut.setShelterInventory("water");
+        
+        String home = hut.toString();
+        System.out.println(home);
+        
+        Location beach = new Location();
+        
+        beach.setColumn(1);
+        beach.setRow(1);
+        beach.setResource("fish");
+        beach.setVisited(true);
+        
+        String place = beach.toString();
+        System.out.println(place);
+        
+        Map firstmap = new Map();
+        
+        firstmap.setColumn(1);
+        firstmap.setRow(1);
+        
+        String start = firstmap.toString();
+        System.out.println(start);
+        
+        Game playerOne = new Game();
+        
+        playerOne.setTotalTime(10);
+        playerOne.setUserProfile("strong");
+        
+        String player = playerOne.toString();
+        System.out.println(player);
     }
-    
+
 }
