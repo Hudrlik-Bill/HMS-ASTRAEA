90a0040db8a688253892b058e69cf707e7488f8c
William Enkey <bill.enkey5@gmail.com>
2017-02-16 22:35:46 -0700

    20170216Final

--- HMS-ASTRAEA/src/HMSASTRAEA/view/GameMenu.java
+++ HMS-ASTRAEA/src/HMSASTRAEA/view/GameMenu.java
@@ -5,15 +5,81 @@
  */
 package HMSASTRAEA.view;
 
+import java.util.Scanner;
+
+
 /**
  *
  * @author Teancum
  */
-public class GameMenu {
+public class GameMenu 
+{
+    private String gameMenu;
 
-    void displayGameMenu() 
+    public void displayGameMenu() 
     {
-        System.out.println("\n  Main Menu:");
+        this.gameMenu = "\nMAIN MENU"
+                      + "\nN - NEW GAME"
+                      + "\nR - RESUME GAME"
+                      + "\nO - OPTIONS"
+                      + "\nH - HELP"
+                      + "\nQ - QUIT";
+        
+        boolean check = false;
+        do 
+        {
+            String userChoice = this.getUserChoice();
+            if (userChoice.toUpperCase().equals("Q"))
+            {
+                return;
+            }
+            
+            check = this.doChoice(userChoice);
+        }
+        while (!check);
     }
-    
+
+    private String getUserChoice() 
+    {
+        System.out.println("\nPlease enter your choice: ");
+        Scanner userInput = new Scanner(System.in);
+        String userChoice = userInput.nextLine();
+        userChoice = userChoice.trim();
+        userChoice = userChoice.toUpperCase();
+        return userChoice;
+    }
+
+    private boolean doChoice(String userChoice) 
+    {
+        switch (userChoice)
+                {
+                    case "N": this.startNewGame();
+                        break;
+                    case "R": this.resumeSavedGame();
+                        break;
+                    case "O": this.displayOptionsMenu();
+                        break;
+                    case "H": this.displayHelpMenu();
+                        break;
+                    default: System.out.println("Invalid selection. Please enter 'N', 'R', 'O', 'H', or 'Q'");
+                }
+        return false;
+    }
+
+    private void startNewGame() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void resumeSavedGame() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void displayOptionsMenu() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void displayHelpMenu() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
