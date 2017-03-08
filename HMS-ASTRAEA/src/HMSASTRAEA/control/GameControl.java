b8532e1d274d319917a5c2d6f8ccb9e780c41988
RussellMarina <marinairussell@gmail.com>
2017-03-07 19:32:37 -0500

    3/7/2017 - Marina's Individual assignment

--- HMS-ASTRAEA/src/HMSASTRAEA/control/GameControl.java
+++ HMS-ASTRAEA/src/HMSASTRAEA/control/GameControl.java
@@ -15,7 +15,14 @@ import HMSASTRAEA.model.Profile;
 public class GameControl 
 {
     private static Game currentGame;
-    private static Game [] savedGames = {new Game(), new Game(), new Game(), new Game(), new Game(), new Game(), new Game(), new Game()};
+    private static Game savedGame1;
+    private static Game savedGame2;
+    private static Game savedGame3;
+    private static Game savedGame4;
+    private static Game savedGame5;
+    private static Game savedGame6;
+    private static Game savedGame7;
+    private static Game savedGame8;
     
     
     public GameControl() {
@@ -57,20 +64,71 @@ public class GameControl
         GameControl.currentGame = currentGame;
     }
 
-    public static Game[] getSavedGames() {
-        return savedGames;
+    public static Game getSavedGame1() {
+        return savedGame1;
     }
 
-    public static void setSavedGames(Game[] savedGames) {
-        GameControl.savedGames = savedGames;
+    public static void setSavedGame1(Game savedGame1) {
+        GameControl.savedGame1 = savedGame1;
     }
+
+    public static Game getSavedGame2() {
+        return savedGame2;
+    }
+
+    public static void setSavedGame2(Game savedGame2) {
+        GameControl.savedGame2 = savedGame2;
+    }
+
+    public static Game getSavedGame3() {
+        return savedGame3;
+    }
+
+    public static void setSavedGame3(Game savedGame3) {
+        GameControl.savedGame3 = savedGame3;
+    }
+
+    public static Game getSavedGame4() {
+        return savedGame4;
+    }
+
+    public static void setSavedGame4(Game savedGame4) {
+        GameControl.savedGame4 = savedGame4;
+    }
+
+    public static Game getSavedGame5() {
+        return savedGame5;
+    }
+
+    public static void setSavedGame5(Game savedGame5) {
+        GameControl.savedGame5 = savedGame5;
+    }
+
+    public static Game getSavedGame6() {
+        return savedGame6;
+    }
+
+    public static void setSavedGame6(Game savedGame6) {
+        GameControl.savedGame6 = savedGame6;
+    }
+
+    public static Game getSavedGame7() {
+        return savedGame7;
+    }
+
+    public static void setSavedGame7(Game savedGame7) {
+        GameControl.savedGame7 = savedGame7;
+    }
+
+    public static Game getSavedGame8() {
+        return savedGame8;
+    }
+
+    public static void setSavedGame8(Game savedGame8) {
+        GameControl.savedGame8 = savedGame8;
+    }
+
     
-    public static void saveSingleGame (Game savedGame, int index){
-        savedGames[index] = savedGame;
-    }
-    public static Game getSingleSavedGame (int index){
-        return savedGames[index];
-    }
 
     
     
--- HMS-ASTRAEA/src/HMSASTRAEA/view/SaveMenuView.java
+++ HMS-ASTRAEA/src/HMSASTRAEA/view/SaveMenuView.java
@@ -18,27 +18,28 @@ public class SaveMenuView extends View
     public SaveMenuView()
     {
                 super(  "\nSAVE MENU: Choose a save slot"
-                      + "\n1 - "+GameControl.getSavedGames()[0].getSaveName()
-                      + "\n2-  "+GameControl.getSavedGames()[1].getSaveName()
-                      + "\n3 - "+GameControl.getSavedGames()[2].getSaveName()
-                      + "\n4 - "+GameControl.getSavedGames()[3].getSaveName()
-                      + "\n5 - "+GameControl.getSavedGames()[4].getSaveName()
-                      + "\n6 - "+GameControl.getSavedGames()[5].getSaveName()
-                      + "\n7 - "+GameControl.getSavedGames()[6].getSaveName()
-                      + "\n8 - "+GameControl.getSavedGames()[7].getSaveName()
+                      + "\n1 - "+GameControl.getSavedGame1().getSaveName()
+                      + "\n2-  "+GameControl.getSavedGame2().getSaveName()
+                      + "\n3 - "+GameControl.getSavedGame3().getSaveName()
+                      + "\n4 - "+GameControl.getSavedGame4().getSaveName()
+                      + "\n5 - "+GameControl.getSavedGame5().getSaveName()
+                      + "\n6 - "+GameControl.getSavedGame6().getSaveName()
+                      + "\n7 - "+GameControl.getSavedGame7().getSaveName()
+                      + "\n8 - "+GameControl.getSavedGame8().getSaveName()
                       + "\nR - Return to Game without Saving");
     }
     @Override
     public boolean doChoice(String userChoice) {
         
     
-    int choice = 0;
-    boolean badFormat = false;
+    
     if(userChoice.equals("R")){
         GameMenuView gameMenu = new GameMenuView();
         gameMenu.displayMenu();
         return false;
     }
+    int choice = 0;
+    boolean badFormat = false;
     try {
         choice = Integer.parseInt(userChoice);
         
@@ -47,23 +48,39 @@ public class SaveMenuView extends View
         badFormat = true;
     }
     if(choice>0 && choice <9){
-        choice--;
         System.out.println("\nPlease enter the name of your saved Game: ");
         Scanner userInput = new Scanner(System.in);
         String newSaveName = userInput.nextLine();
         newSaveName = newSaveName.trim();
         newSaveName = newSaveName.toUpperCase();
-        //GameControl.getCurrentGame().setSaveName(newSaveName);
-        //System.out.println(saveName);
-        //System.out.println(GameControl.getCurrentGame().getSaveName());
-        //currentSavedGames[choice-1] = GameControl.getCurrentGame();
-        //System.out.println(currentSavedGames[choice-1].getSaveName());
-        //System.out.println(currentSavedGames[choice-2].getSaveName());
-        GameControl.saveSingleGame(GameControl.getCurrentGame(),choice);
-        GameControl.getSingleSavedGame(choice).setSaveName(newSaveName);
+        GameControl.getCurrentGame().setSaveName(newSaveName);
+        
+        switch(choice){
+            case 1: GameControl.setSavedGame1(GameControl.getCurrentGame());
+            break;
+            case 2: GameControl.setSavedGame2(GameControl.getCurrentGame());
+            break;
+            case 3: GameControl.setSavedGame3(GameControl.getCurrentGame());
+            break;
+            case 4: GameControl.setSavedGame4(GameControl.getCurrentGame());
+            break;
+            case 5: GameControl.setSavedGame5(GameControl.getCurrentGame());
+            break;
+            case 6: GameControl.setSavedGame6(GameControl.getCurrentGame());
+            break;
+            case 7: GameControl.setSavedGame7(GameControl.getCurrentGame());
+            break;
+            case 8: GameControl.setSavedGame8(GameControl.getCurrentGame());
+            break;
+            default:
+            break;
+        }
         GameMenuView gameMenu = new GameMenuView();
         gameMenu.displayMenu();
     }
+        
+        
+    
     else{
         System.out.println("Invalid selection. Please enter 1-8 or 'R'");
     }
